db3cb0dce880c818ce4f1665d38546b96e4780c7
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2024-01-02 16:14:44 -0300

    lesson 2 finished (attatch behavior)

---
+++ src/models/WhishList.test.js
@@ -0,0 +1,46 @@
+import { WishList, WishListItem } from "./WishList";
+
+it("can create a instance of a model", () => {
+	const item = WishListItem.create({
+		name: "Chronicles of Narnia Box Set - C.S. Lewis",
+		price: 28.73,
+	});
+
+	expect(item.price).toBe(28.73);
+	expect(item.image).toBe("");
+
+	item.changeName("Narnia");
+
+	expect(item.name).toBe("Narnia");
+});
+
+it("can create a wishlist", () => {
+	const list = WishList.create({
+		items: [
+			{
+				name: "Chronicles of Narnia Box Set - C.S. Lewis",
+				price: 28.73,
+				image:
+					"https://images-na.ssl-images-amazon.com/images/I/51RY40V%2BcNL._SX406_BO1,204,203,200_.jpg",
+			},
+		],
+	});
+
+	expect(list.items.length).toBe(1);
+	expect(list.items[0].price).toBe(28.73);
+});
+
+it("can add new items", () => {
+	const list = WishList.create();
+	list.add(
+		WishListItem.create({
+			name: "Chesterton",
+			price: 10,
+		})
+	);
+
+	expect(list.items.length).toBe(1);
+	expect(list.items[0].name).toBe("Chesterton");
+	list.items[0].changeName("Book of G.K. Chesterton");
+	expect(list.items[0].name).toBe("Book of G.K. Chesterton");
+});
---
+++ src/models/WishList.js
@@ -0,0 +1,29 @@
+import { types } from "mobx-state-tree";
+
+export const WishListItem = types
+	.model({
+		name: types.string,
+		price: types.number,
+		image: types.optional(types.string, ""),
+	})
+	.actions((self) => ({
+		changeName(newName) {
+			self.name = newName;
+		},
+		changePrice(newPrice) {
+			self.price = newPrice;
+		},
+		changeImage(newImage) {
+			self.image = newImage;
+		},
+	}));
+
+export const WishList = types
+	.model({
+		items: types.optional(types.array(WishListItem), []),
+	})
+	.actions((self) => ({
+		add(item) {
+			self.items.push(item);
+		},
+	}));
