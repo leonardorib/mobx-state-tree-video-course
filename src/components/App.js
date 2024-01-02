4bd9794fdfbc69e77fa4c7b5a0475a45523becdd
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2024-01-02 19:39:13 -0300

    lesson 5 - rendering models

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,15 @@
 import logo from "../assets/santa-claus.png";
 import "../assets/index.css";
+import WishListView from "./WishListView";
 
-function App() {
+function App(props) {
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
+				<h1>WishList</h1>
 			</header>
+			<WishListView wishList={props.wishList} />
 		</div>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,32 @@ import ReactDOM from "react-dom/client";
 import "./assets/index.css";
 import App from "./components/App";
 
+import { WishList } from "./models/WishList";
+
+const wishList = WishList.create({
+	items: [
+		{
+			name: "LEGO Mindstorms EV3",
+			price: 349.95,
+			image:
+				"https://images-na.ssl-images-amazon.com/images/I/71CpQw%2BufNL._SL1000_.jpg",
+		},
+		{
+			name: "Miracles - C.S. Lewis",
+			price: 12.91,
+			image:
+				"https://images-na.ssl-images-amazon.com/images/I/41JC54HEroL._SX331_BO1,204,203,200_.jpg",
+		},
+	],
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App />
+		<App wishList={wishList} />
 	</React.StrictMode>
 );
+
+setInterval(() => {
+	wishList.items[0].changePrice(wishList.items[0].price + 1);
+}, 1000);
