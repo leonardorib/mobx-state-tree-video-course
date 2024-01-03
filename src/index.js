2c80fa14369f2f6405bf7adfa05516edf78324a1
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2024-01-02 21:05:56 -0300

    lesson 9 - persisting the state with localStorage

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import App from "./components/App";
 
 import { WishList } from "./models/WishList";
 
-const wishList = WishList.create({
+import { onSnapshot } from "mobx-state-tree";
+
+let initialState = {
 	items: [
 		{
 			name: "LEGO Mindstorms EV3",
@@ -20,6 +22,17 @@ const wishList = WishList.create({
 				"https://images-na.ssl-images-amazon.com/images/I/41JC54HEroL._SX331_BO1,204,203,200_.jpg",
 		},
 	],
+};
+
+if (localStorage.getItem("wishlistapp")) {
+	const json = JSON.parse(localStorage.getItem("wishlistapp"));
+	if (WishList.is(json)) initialState = json;
+}
+
+const wishList = WishList.create(initialState);
+
+onSnapshot(wishList, (snapshot) => {
+	localStorage.setItem("wishlistapp", JSON.stringify(snapshot));
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
